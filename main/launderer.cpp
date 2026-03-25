7199b5d6cf2e7403a3adfbc88b29258db11c665d
trevorswan11 <tcs94@case.edu>
2026-03-25 12:19:09 -0400

    Properly calibrated

--- main/launderer.cpp
+++ main/launderer.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <cstdint>
 #include <optional>
 #include <print>
@@ -39,23 +40,17 @@ dev::DigitalPinIn white_reset{27, idf::GPIOPullMode::PULLDOWN()};
 dev::PwmOut   pump{dev::pin_layout::A0};
 dev::AnalogIn pressure_sensor{dev::pin_layout::A1};
 
-// Calibrated in lab (# Go stones, Zeroed value)
-// (10, 12)
-// (20, 23)
-// (30, 35)
-// (40, 46)
-// (50, 55)
-// (60, 64)
-// (70, 74)
-// (80, 81)
-// (90, 90)
-// (100, 99)
-// (110, 104)
-// (120, 110)
-// (130, 117)
-// (140, 122)
+// From desmos fit
+constexpr float FIT_A        = -478286.117341f;
+constexpr float FIT_B        = 830.395033677f;
+constexpr float FIT_C        = 580.401879334f;
+constexpr float STONE_WEIGHT = 2.5f;
 
-[[nodiscard]] auto stones_from_adc(i32 zeroed) -> float;
+[[nodiscard]] auto weight_from_adc(i32 zeroed) noexcept -> float {
+    if (zeroed <= 0) { return 0.0f; }
+    const auto stones = FIT_A / (zeroed - FIT_C) - FIT_B;
+    return stones * STONE_WEIGHT;
+}
 
 extern "C" auto app_main() -> void {
     lcd.init();
@@ -109,6 +104,9 @@ extern "C" auto app_main() -> void {
             tare_offset = current_average();
             tared       = true;
         } else if (tared) {
+            const auto weight = weight_from_adc(zeroed);
+            std::println("weight={}", weight);
+
             if (red) {
                 dirt.emplace(Dirtiness::NORMAL);
             } else if (blue) {
